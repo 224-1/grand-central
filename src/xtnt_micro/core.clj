f51fc16151e454ed9cf7322e8bfaff65d81ab159
ParikshithMohan <parikshithmohan@gmail.com>
2019-07-22 14:49:04 +0530

    Issue #5 All Form elements implemented

--- src/xtnt_micro/core.clj
+++ src/xtnt_micro/core.clj
@@ -20,8 +20,64 @@
   (html5 {:lang "en"}
          [:body [:h1 "Boilerplate for Clojure Ring based server"]]))
 
+(defn forms
+	[request]
+	(html5 {:lang "en"}
+		     [:body
+          "Form element 1 (Form and input)"
+          [:div.form1
+           [:form "First Name:"
+            [:br]
+            [:input {:type "text" :name "firstname" :value "pk"}]
+            [:br]
+            "Last Name:"
+            [:br]
+            [:input {:type "text" :name "firstname" :value "mo"}]
+            [:br]
+            [:input {:type "submit" :name "Submit"}]]
+           ]
+          [:br]
+          "Form element 2 (select and option)"
+          [:br]
+          [:div.form2
+           [:select {:name "Cars" :size "2"}
+            [:option {:value "volvo"} "VOLVO"]
+            [:option {:value "fiat"} "FIAT"]
+            [:option {:value "ford"} "FORD"]
+            ]
+           ]
+          [:br]
+          "Form element 3 (Button)"
+          [:br]
+          [:div.form3
+           [:textarea {:name "message" :rows "10" :cols "50"}]
+           ]
+          ]
+         [:br] "Form element 4" [:br]
+         [:div.form4
+          [:button {:on-click [:p "hello"]} "Click Me!!!"]
+          ]
+         [:br]
+         "Form element 5 (datalist)"
+         [:br]
+         [:div.form5
+          [:form
+           [:input {:list "browsers"}]
+           [:datalist {:id "browsers"}
+            [:option {:value "Chrome"}]
+            [:option {:value "Firefox"}]
+            [:option {:value "Opera"}]
+            [:option {:value "Safari"}]
+            ]
+           [:button "submit"]
+           ]
+          ]
+	       )
+  )
+
 (defroutes app
   (GET "/" [] boiler-plate)
+  (GET "/forms" [] forms)
   (not-found "<h1>Page not found</h1>"))
 
 (defn -main
