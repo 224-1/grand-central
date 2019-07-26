2716056bf01a8fa98454adb516e8ce0a393d772a
VikramShawFL <vikram@front.live>
2019-07-26 15:55:31 +0530

    Initial commit

--- project.clj
+++ project.clj
@@ -4,12 +4,28 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ring "1.7.1"]
+                 [ring/ring-json "0.4.0"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-devel "1.7.1"]
                  [compojure "1.6.1"]
+                 [ring-cors "0.1.13"]
+                 ;;To run the server we need to add jetty-adapter
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 ;;For clojurescript
+                 [org.clojure/clojurescript "1.10.520"]
                  [hiccup "1.0.5"]]
 
   :repl-options {:init-ns xtnt-micro.core}
-  :main xtnt-micro.core
-  :profiles {:dev
-             {:main xtnt-micro.core/-dev-main}
-             })
+  ;;For clojurescript you need to add this
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :source-paths ["src/clj"]
+  :cljsbuild {
+              :builds [{
+                        :source-path "src/cljs"
+                        :compiler {
+                                   :output-to "resources/public/xtnt-micro.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
+
+
+  :main xtnt-micro.core)
---
+++ src/cljs/xtnt_micro/core.cljs
@@ -0,0 +1,25 @@
+(ns xtnt-micro.core)
+
+[:html
+ [:head
+  [:script {:type "text/javascript", :src "https://www.google.com/jsapi"}]
+  [:script {:type "text/javascript"
+
+            (def data #js [])
+            (def timestamps #js [])
+
+            (def socket (js/WebSocket.  "ws://localhost:8080/happiness"))
+
+            (set!
+             (.-onmessage socket)
+             (fn [event]
+               (.push data (aget (.parse js/JSON (.-data js/event)) "happiness"))
+               (.push timestamps (new Date))))
+
+            (defn zip
+              [arrays]
+              (.map (aget arrays 0) (fn [_ i] (.map arrays (fn [array] (aget array i))))))}]]
+ [:body
+  [:div {:id "chart", :style "height: 300px;"}]
+  ]]
+
