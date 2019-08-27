064e454238ba6015c965981b5d4468aebf0f896b
ParikshithMohan <parikshithmohan@gmail.com>
2019-08-27 22:56:04 +0530

    Issue #17 Updated Newsfeed created

--- src/clj/xtnt_micro/newsfeed.clj
+++ src/clj/xtnt_micro/newsfeed.clj
@@ -19,14 +19,14 @@
     [:div.wrap
      [:div.sidenav {:class "three columns"}
       [:img.logo {:src "https://i.imgur.com/cD7suBe.png"}]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/50/000000/home.png" :width "25px"} "Home"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/48/000000/hashtag.png" :width "25px"} "Explore"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/appointment-reminders.png" :width "25px"} "Notifications"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/100/000000/envelope-number.png" :width "25px"} "Messages"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/bookmark-ribbon.png" :width "25px"} "Bookmarks"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/list.png" :width "25px"} "Lists"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/contract-job.png" :width "25px"} "Profile"]]
-      [:a#listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/50/000000/more.png" :width "25px"} "More"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/50/000000/home.png" :width "25px"} "Home"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/48/000000/hashtag.png" :width "25px"} "Explore"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/appointment-reminders.png" :width "25px"} "Notifications"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/100/000000/envelope-number.png" :width "25px"} "Messages"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/bookmark-ribbon.png" :width "25px"} "Bookmarks"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/list.png" :width "25px"} "Lists"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/64/000000/contract-job.png" :width "25px"} "Profile"]]
+      [:a.listlogo {:href "#"} [:img {:src "https://img.icons8.com/ios-filled/50/000000/more.png" :width "25px"} "More"]]
       [:button.button2 [:img {:src "https://i.imgur.com/CSx4pOj.png" :width "15px"}] "Create feed"]
       ]
      ]
@@ -160,4 +160,3 @@
      ];;rightcontainer end
     ]
    ))
-
