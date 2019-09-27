e265b2527a61e58af0a0683d1575fa22261881fd
ParikshithMohan <parikshithmohan@gmail.com>
2019-09-27 17:51:19 +0530

    Issue #38 DiscordUI final - fully componentized

--- src/clj/xtnt_micro/discordui.clj
+++ src/clj/xtnt_micro/discordui.clj
@@ -1,204 +1,206 @@
 (ns xtnt-micro.discordui
-  (:use
-   [ring.adapter.jetty :as jetty]
-   [hiccup.core :refer :all]
-   [hiccup.page :refer :all]
-   [compojure.core :refer [defroutes GET]]
-   [compojure.route  :refer [resources]]
-   [ring.adapter.jetty :as jetty]
-   [ring.middleware.reload :refer [wrap-reload]]))
+  (:require [ring.adapter.jetty :as jetty]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route  :refer [resources]]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
-(def state (atom
-            {:user_name "Parikshith"
-             :user_id "#6228"
-             :user_logo "https://i.imgur.com/6SRX0td.png"
-             :channel {:xtnt{:name "XTNT"
-                             :room ["xtnt-micro","horizon","horizon-system-t","horizon-t19","dev-support","lounge","forge-music","forge-controller"]
-                             :voice
-                             ["lounge","support","training","dev","r&d","testing & documentation","ui/ux","backend & db"]}}
-             :members{
-                      :product{:count "2"
-                               :username ["azureus","hobbes"]
-                               }
+(def active-server (atom :0))
+(defonce state (atom {:user_name "Parikshith"
+               :user_id "#6228"
+               :user_logo "https://i.imgur.com/6SRX0td.png"
+               :server {:0 {:name "XTNT"
+                            :rooms {:name "Interaction room"
+                                    :list {:0 "xtnt-micro"
+                                           :1 "horizon"
+                                           :2 "horizon-system-t"
+                                           :3 "horizon-t15"
+                                           :4 "dev-support"
+                                           :5 "lounge"
+                                           :6 "forge-music"
+                                           :7 "forge-controller"}
+                                    :active :0
+                                    :logo "https://i.imgur.com/N0zt5ji.png"}
+                            :voice {:name "voice channel"
+                                    :list {:0 "lounge"
+                                           :1 "support"
+                                           :2 "training"
+                                           :3 "dev"
+                                           :4 "r&d"
+                                           :5 "ui/ux"
+                                           :6 "backend & db"}
+                                    :active nil
+                                    :logo "https://i.imgur.com/wpabDPl.png"}}}
+               :members{:product ["azureus","hobbes"]
+                        :dev ["Parikshith","Vikram","kavya","baishakhi"]
+                        :intern ["Noobno1","cheesepopcorn99","vazir"]
+                        :offline ["Amadeus","GSD47","Vicaay","Sushil"]}
+               :logos {:azureus "https://i.imgur.com/zn6kLLT.png"
+                       :hobbes "https://i.imgur.com/0FBdAYu.png"
+                       :Parikshith "https://i.imgur.com/S1Tc0oi.png"
+                       :Vikram "https://i.imgur.com/NUNWrWP.png"
+                       :kavya "https://i.imgur.com/8A9JwAO.png"
+                       :baishakhi "https://i.imgur.com/zn6kLLT.png"
+                       :Noobno1 "https://i.imgur.com/0FBdAYu.png" 
+                       :cheesepopcorn99 "https://i.imgur.com/zn6kLLT.png"
+                       :vazir "https://i.imgur.com/zn6kLLT.png"
+                       :Amadeus "https://i.imgur.com/8A9JwAO.png"
+                       :GSD47 "https://i.imgur.com/NUNWrWP.png"
+                       :Vicaay "https://i.imgur.com/S1Tc0oi.png"
+                       :Sushil "https://i.imgur.com/0FBdAYu.png"}
+               :color ["#e01f63","#117e67","#8E9297","#9b59b6","#3397da"]}))
 
-                      :dev {:count "4"
-                            :username ["Parikshith","Vikram","kavya","baishakhi"]
-                            }
+(defonce messages (atom {:0 {:server 0
+                         :channel :0
+                         :sender "Parikshith"
+                         :subject "[224-1/xtnt-micro] New comment on issue #35: Migrate trello page from clojure to clojurescript"
+                         :content "Context: Trying to add multiple outputs/targets in project-cljsbuild Issue: while adding multiple targets,facing issue. Logs: Loading c:/Users/welcome/AppData/Roaming/.emacs.d/core/core-load-paths.el (source)...done Loading c:/Users/welcome/AppData/Roaming/.spacemacs...done Setting the font...  (Spacemacs) Warning: Cannot find any of the specified fonts (Source Code Pro)! Font settings may not be correct. PNG warning: Interlace handling should be turned on when using png_rea...(Spacemacs) Warning: Cannot find any of the specified fonts (Source Code Pro)! Font settings may not be correct. PNG warning: Interlace handling should be turned on when using png_rea..."
+                         :timestamp "Last Friday 11:39PM"}}))
 
-                      :intern {:count "3"
-                               :username ["Noobno1","cheesepopcorn99","vazir"]
-                               }
+;; Logo getter 
+(defn get_logo [user]
+  (let [usr (keyword user)]
+    (->> @state :logos usr)))
 
-                      :offline {:count "4"
-                                :username ["Amadeus","GSD47","Vicaay","Sushil"]}}
-             :logos ["https://i.imgur.com/zn6kLLT.png",
-                     "https://i.imgur.com/0FBdAYu.png",
-                     "https://i.imgur.com/S1Tc0oi.png",
-                     "https://i.imgur.com/NUNWrWP.png",
-                     "https://i.imgur.com/8A9JwAO.png"]}))
-
+;; page header
 (defn head []
   [:head (include-css "css/discordui.css" "css/skeleton.css" "css/normalize.css")
    [:title "DISCORD-UI"]
    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
    [:link {:href "https://fonts.googleapis.com/css?family=Lato&display=swap" :rel "stylesheet"}]])
 
+;; Discord Home button
 (defn home []
-  [:div#home.c1_ele [:img {:src "https://i.imgur.com/YD8cAku.png" :width "51px"}]])
+  [:div#home.c1_ele])
 
+;; Server Panel
 (defn container_1 []
   [:div.container_1
    (home)
    [:hr.c1_hr ]
-   [:div#forge_channel.c1_ele [:img {:src "https://i.imgur.com/5Nddaj9.png" :width "51px"}]]
-   [:div#xtnt_channel.c1_ele.left-border [:img {:src "https://i.imgur.com/5qpZZMo.png" :width "51px" :style "border-radius:15px;"}]]
-   [:div#plus_c1.c1_ele.c1_op [:img {:src "https://i.imgur.com/L0oSuHh.png" :width "22px"}]]
-   [:div#search_c1.c1_ele.c1_op [:img {:src "https://i.imgur.com/bBN1Xwg.png" :width "22px"}]]
+   [:div#forge_channel.c1_ele]
+   [:div#xtnt_channel.c1_ele.left-border ]
+   [:div#plus_c1.c1_ele.c1_op ]
+   [:div#search_c1.c1_ele.c1_op]
    [:hr.c1_hr]
-   [:div#download_c1.c1_ele.c1_op [:img {:src "https://i.imgur.com/phXJURZ.png" :width "22px"}]]])
+   [:div#download_c1.c1_ele.c1_op ]])
 
+;; Active Server
 (defn container_2_head []
   [:div.container_2_head
-   [:div.c2_head
-    [:p (-> @state :channel :xtnt :name)]
-    [:img#c2_expand {:src "https://i.imgur.com/Nzf1e1i.png" :width "24px"}]]])
+  [:div.c2_head
+   [:p (-> @state :server (@active-server) :name)]
+   [:div#c2_expand]]])
 
-(defn container_2_rooms []
-  (map #(identity [:div.c2_rooms
-                   [:img {:src "https://i.imgur.com/N0zt5ji.png" :width "23px"} [:ul [:li %]]]
-                   [:br]])
-       (-> @state :channel :xtnt :room)))
+;;Channel lists and rooms organization function
+(defn channel_list [arg]
+  (let [current (-> @state :server (@active-server) arg)]
+    (map #(identity [:div.c2_rooms
+                     [:img#channel_logo {:src (:logo current)} [:ul [:li (val %)]]]
+                     [:br]]) (-> current :list))))
 
-(defn container_2_voices []
-  (map #(identity [:div.c2_rooms
-                   [:img {:src "https://i.imgur.com/wpabDPl.png" :width "23px"} [:ul [:li %]]]
-                   [:br]])
-       (-> @state :channel :xtnt :voice)))
-
+;;User Details and options
 (defn container_2_footer []
   [:div.container_2_footer
    [:div.user_details
-    [:img#c2_userlogo {:src (:user_logo @state) :width "35px"}]
+    [:img#c2_userlogo {:src (:user_logo @state)}]
     [:p#c2_username (:user_name @state)]
     [:p#c2_userid (:user_id @state)]]
-   [:div#settings.c2_footer_icon [:img {:src "https://i.imgur.com/eBKL08g.png" :width "19px"}]]
-   [:div#headphones.c2_footer_icon [:img {:src "https://i.imgur.com/BMf07ds.png" :width "19px"}]]
-   [:div#mic.c2_footer_icon [:img {:src "https://i.imgur.com/aSmfWmk.png" :width "19px"}]]
-   ])
+   [:div#settings.c2_footer_icon]
+   [:div#headphones.c2_footer_icon]
+   [:div#mic.c2_footer_icon]])
 
+;;Channel list & user details section
 (defn container_2 []
   [:div.container_2
    (container_2_head)
    [:div.c2_channels
     [:div.interaction_room
      [:div.c2_channel_heads
-      [:img {:src "https://i.imgur.com/oRG1PtF.png" :width "14px"}]
+      [:div#channel_expand]
       [:p "interaction room"]]
-     (container_2_rooms)]
+     (channel_list :rooms)]
     [:div.voice_channel
      [:div.c2_channel_heads
-      [:img {:src "https://i.imgur.com/oRG1PtF.png" :width "14px"}]
+      [:div#channel_expand]
       [:p "voice channels"]]
-     (container_2_voices)]]
+     (channel_list :voice)]]
    (container_2_footer)])
 
+;;Top bar
 (defn main_header []
   [:div.main_header
-   [:img#mh_hash.mh_channel_name {:src "https://i.imgur.com/N0zt5ji.png" :width "26px"}
-     [:p#mh_p.mh_channel_name (-> @state :channel :xtnt :room first)]]
+   [:img#mh_hash.mh_channel_name {:src (-> @state :server (@active-server) :rooms :logo)}
+     [:p#mh_p.mh_channel_name (-> @state :server (@active-server) :rooms :list (@active-server))]]
    [:div.main_header_icons
-    [:div#help.mh_right_icon [:img {:src "https://i.imgur.com/OeBFn9V.png" :width "22px"}]]
-    [:div#mentions.mh_right_icon [:img {:src "https://i.imgur.com/PxV9PsS.png" :width "21px"}]]
+    [:div#help.mh_right_icon]
+    [:div#mentions.mh_right_icon]
     [:input#searchbar.mh_right_icon {:type "search" :placeholder "Search"}]
-    [:div#members.mh_right_icon [:img {:src "https://i.imgur.com/FaMlvnI.png" :width "24px"}]]
-   [:div#pinned.mh_right_icon [:img {:src "https://i.imgur.com/xOeGTTW.png" :width "24px"}]]
-   [:div#notification.mh_right_icon [:img {:src "https://i.imgur.com/WFH5qXj.png" :width "24px"}]]]])
+    [:div#members.mh_right_icon]
+   [:div#pinned.mh_right_icon]
+   [:div#notification.mh_right_icon]]])
 
+;;Middle section chat messages
 (defn chat_holder []
   [:div.chat_holder
-   [:img#github_img {:src "https://i.imgur.com/QCHNgYc.png" :width "40px"}]
+   [:div#github_img]
    [:div#github_head
-    [:p#github_tag "GitHub" [:img {:src "https://i.imgur.com/mFNoGHB.png" :width "30px"}]]
-    [:p#github_date "Last Friday at 5:23 PM"]]
+    [:div#github_tag
+     [:p "GitHub"
+      [:img {:src "https://i.imgur.com/mFNoGHB.png"}]]]
+    [:p#github_date (-> @messages (@active-server) :timestamp)]]
    [:div.chat_received
     [:div.chat_received_head
-     [:img#chat_userlogo {:src "https://i.imgur.com/APLLPNU.png" :width "25px"}]
-     [:p#chat_username (:user_name @state)]
-     [:p#chat_content_head "[224-1/xtnt-micro] New comment on issue #35: Migrate trello page from clojure to clojurescript"]
-     [:div#chat_content
-     [:p "@azureus89 tried with trampoline also,followed the steps cider-jack-in-cljs -->figwheel-main later dev,Error"]
-     [:p "[Figwheel] Validating figwheel-main.edn
-(ns xtnt-auth.datasource
-  (:require [hikari-cp.core :refer :all]))
-(def datasource-options {:adapter
-(defn get-ds []
-  (defonce ds (make-datasource datasource-options))
-  {:datasource ds})
-Don't know how to create ISeq from: clojure.lang.Keyword"]
-      [:p"you are not using any database right now, you're trying to connect to one that isn't there, look up h2 .."]]]]])
+     [:div#chat_userlogo]
+     [:p#chat_username (-> @messages (@active-server) :sender)]
+     [:p#chat_content_head (-> @messages (@active-server) :subject)]
+     [:div#chat_content [:p (-> @messages (@active-server) :content)]]]]])
 
+;;Whole middle section with messages typing
 (defn container_3 []
   [:div.container_3
    [:div.c3_contents
     (chat_holder)
-    (chat_holder)
-    ]
+    (chat_holder)]
    [:div.messaging
-    [:div#plus_c3 [:img {:src "https://i.imgur.com/KNUm5uW.png" :width "24px"}]]
+    [:div#plus_c3]
     [:div#type_message [:input {:type "text" :placeholder "Message #xtnt-micro"}]]
-    [:div#smiley_c3.messaging_right_icon [:img {:src "https://i.imgur.com/jYWODeK.png" :width "24px"}]]
-    [:div#gif_c3.messaging_right_icon [:img {:src "https://i.imgur.com/FKFEuzz.png" :width "40px"}]]
-    [:div#gift_c3.messaging_right_icon [:img {:src "https://i.imgur.com/ZOUVr8g.png" :width "23px"}]]]])
+    [:div#smiley_c3.messaging_right_icon]
+    [:div#gif_c3.messaging_right_icon]
+    [:div#gift_c3.messaging_right_icon]]])
 
-(defn rand-logo []
-  (rand-nth (-> @state :logos)))
+;;Member list organization function
+(defn members_list [arg]
+  (let [current (-> @state :members arg)]
+    [:div.members_list (map #(identity [:div.members_content
+                     [:img#member_logo {:src (get_logo %)} [:p {:style (str "color:" (rand-nth (-> @state :color))";")} %]]
+                     [:br]]) (-> current))]))
 
-(def rand-color ["#e01f63","#117e67","#dcddde","#9b59b6","#3397da"])
+;;Member counting function
+(defn member_counter [arg]
+  (let [counter (-> @state :members arg)]
+    (str (count (-> counter)))))
 
-(defn product_c4 []
-  [:div.product_c4
-   [:div.product_head
-    [:p.c4_header "Product -" (-> @state :members :product :count)]]
-   (map #(identity [:div.product_members
-                    [:img {:src (rand-logo) :width "35px"} [:p {:style (str "color:" (rand-nth rand-color)";")} %]]
-                      [:br]])
-          (-> @state :members :product :username))])
-
-(defn dev_c4 []
-  [:div.dev_c4
-   [:div.dev_head
-    [:p#dev_p.c4_header "Dev - " (-> @state :members :dev :count)]]
-   (map #(identity [:div.dev_members
-                    [:img {:src (rand-logo) :width "35px"} [:p {:style (str "color:" (rand-nth rand-color)";")} %]]
-                    [:br]])
-        (-> @state :members :dev :username))])
-
-(defn intern_c4 []
-  [:div.intern_c4
-   [:div.intern_head
-    [:p#intern_p.c4_header "Intern - " (-> @state :members :intern :count)]]
-   (map #(identity [:div.intern_members
-                    [:img {:src (rand-logo) :width "35px"} [:p {:style (str "color:" (rand-nth rand-color)";")} %]]
-                    [:br]])
-        (-> @state :members :intern :username))])
-
-(defn offline_c4 []
-  [:div.offline_c4
-   [:div.offline_head
-    [:p#offline_p.c4_header "Offline - " (-> @state :members :offline :count)]]
-   (map #(identity [:div.offline_members
-                    [:img {:src (rand-logo) :width "35px"} [:p {:style (str "color:" (rand-nth rand-color)";")} %]]
-                    [:br]])
-        (-> @state :members :offline :username))])
-
+;;All group member list-Right most container
 (defn container_4 []
   [:div.container_4
    [:div
-    (product_c4)
-    (dev_c4)
-    (intern_c4)
-    (offline_c4)]])
+    [:div.members_list_head.product
+     [:p.c4_header "Product- " (member_counter :product)]
+     (members_list :product)]
+    [:div.members_list_head.dev
+     [:p.c4_header "Dev- " (member_counter :dev)]
+     (members_list :dev)]
+    [:div.members_list_head.intern
+     [:p.c4_header "intern- " (member_counter :intern)]
+    (members_list :intern)]
+    [:div.members_list_head.offline
+     [:p.c4_header "Offline- " (member_counter :offline)]
+    [:div#offline(members_list :offline)]]]])
 
+;;Main function
 (defn discord_ui
   [request]
   (html5
