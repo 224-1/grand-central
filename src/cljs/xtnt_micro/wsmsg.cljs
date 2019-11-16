19ea7d9cd80e881922e74981272ffdcff8105ed5
Sidharth Sudarshan <sidharth.sudarshan@gmail.com>
2019-11-16 05:40:05 +0530

    Simple Websocket Example - Removed chord from dependencies (was using deprecated functions in http-kit/ old version "2.1.19" - main cause of inconsistency in send/recieve behaviour) - removed files / lines of code that were hence redundant - Removed core.async based implementation (should integrate later after benchmarking standard websocket implementation - reducing immediate complexity) - BREAKING UPDATE! Commented out current chat handler - this will break chat example on "/" route in dev build - Created basic websocket handler example (ws-handler) in handler.clj - TODO reimplement chat handler on top of current example in future issues - TODO add documentation for extending the websocket handler for use in real world applications (data format required from client - and event handler functions to be implemented on the server) - TODO cljs client implementation to show interaction with websocket

--- src/cljs/xtnt_micro/wsmsg.cljs
+++ src/cljs/xtnt_micro/wsmsg.cljs
@@ -1,9 +1,5 @@
-
 (ns xtnt-micro.wsmsg
-  (:require [reagent.core :as reagent :refer [atom]]
-            [chord.client :refer [ws-ch]]
-            [cljs.core.async :as async])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (goog-define websocket-url "ws://localhost:8000/msg")
 
@@ -11,21 +7,4 @@
 
 (enable-console-print!)
 
-(println @messages)
-
-;; (defn msg-data []
-;;   [:div
-;;    (into [:ul] (map #(vector :li %) @messages))])
-
-(go
-  (let [{:keys [ws-channel error]} (async/<! (ws-ch websocket-url))]
-    (if-not error
-      (async/>! ws-channel "Hello server from client")
-      (println error))))
-
-(go
-  (let [{:keys [ws-channel]} (async/<! (ws-ch websocket-url))
-        {:keys [message]} (async/<! ws-channel)]
-    (println "got message from server:" message)))
-
-;; (reagent/render-component [msg-data] (. js/document (getElementById "app")))
+;; try weavejester/haslett as a cljs websocket library
