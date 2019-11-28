b6c0270d9d2cad801c9e8d77c5b2c19dd8961eed
sahabaishakhi <baishakhi@front.live>
2019-11-29 04:07:15 +0530

    clojurescript update dom error #59

--- src/cljs/xtnt_micro/websclient.cljs
+++ src/cljs/xtnt_micro/websclient.cljs
@@ -9,11 +9,40 @@
 
 (enable-console-print!)
 
+(defonce messages (atom {}))
+
+(defn msg-typed []
+  [:div (into [:ul] (map #(vector :li %) @messages))])
+
+(defn msg-data []
+  [:div#test
+   [:form
+    ;; {:on-submit (fn [x]
+    ;;               (.preventDefault x)
+    ;;               (reset! messages nil))}
+    [:input#msgval {:type "text"
+             :on-change #(reset! messages (-> % .-target .-value))
+             }]
+    ;; [:input {:type "submit" :onClick #(reset! messages (-> % .-target .-value))}]
+    ;; [:button {:onclick } "Send"]
+    ]
+   ]
+  )
+
+(defn update-msg []
+  (let [div (-> js/document
+                .-getElementById "msgval")]
+    (js/console.log (. div -value))))
+
+(update-msg)
+
+(defn msg-body []
+  [:div
+   (msg-typed)
+   (msg-data)])
+
 ;; try weavejester/haslett as a cljs websocket library
-(go (let [stream (<! (ws/connect
-                      websocket-url
-                      {:format fmt/json}
-                      ))]
+(go (let [stream (<! (ws/connect websocket-url {:format fmt/json}))]
       (>!
        (:sink stream)
        {:type "echo" :data "something"}
@@ -22,3 +51,5 @@
        )
       (println (<! (:source stream)))
       (ws/close stream)))
+
+(reagent/render-component [msg-body] (. js/document (getElementById "app")))
